1dd5551454aec840dd844ba8dabd53fd9658f47b
eSam0531 <esama01@jaguar.tamu.edu>
2022-02-04 08:52:20 -0600

    edit to linked list object and items contained

--- linkedlistassignment/src/com/hcl/linkedlistassignment/LinkedListAssignment.java
+++ linkedlistassignment/src/com/hcl/linkedlistassignment/LinkedListAssignment.java
@@ -5,39 +5,65 @@ import java.util.LinkedList;
 public class LinkedListAssignment {
 
 	public static void main(String[] args) {
-		LinkedList<String> cars = new LinkedList<String>();
-		cars.add("Volvo");
-		cars.add("Jaguar");
-		cars.add("Toyota");
-		cars.add("Dodge");
-		cars.add("Ford");
-		cars.add("Audi");
-		cars.add("Lexus");
-		cars.add("BMW");
-		cars.add("Jeep");
+		LinkedList<String> dogs = new LinkedList<>();
+		dogs.add("American Pitbull Terrier");
+		dogs.add("Dalmation");
+		dogs.add("German Shepard");
+		dogs.add("Boxer");
+		dogs.add("Labrador Retriever");
+		dogs.add("Poodle");
+		dogs.add("Dobermann");
+		dogs.add("Shiba Inu");
+		dogs.add("Belgian Malionis");
 		
 		// print all
-		System.out.println(cars);
+		System.out.println("All breeds:");
+		printAllelements(dogs);
+		newLine();
 		
 		//use getFirst() to display the first item in the list
-		System.out.println(cars.getFirst());
+		System.out.println("Breed at first element");
+		System.out.println(dogs.getFirst());
+		newLine();
 		
 		//use removeFirst() to remove the first item from the list
-		cars.removeFirst();
-		System.out.println(cars);
+		dogs.removeFirst();
+		System.out.println("All breeds after removal of first element");
+		printAllelements(dogs);
+		newLine();
+		
+		//user addFirst() to add at the first element
+		dogs.addFirst("Border Collie");
+		System.out.println("All breeds after addition at first element");
+		printAllelements(dogs);
+		newLine();
 		
 		//use getLast() to display the last item in the list
-		System.out.println(cars.getLast());
+		System.out.println("Breed at last element");
+		System.out.println(dogs.getLast());
+		newLine();
 		
 		//use addLast() to add to end of linked list
-		cars.addLast("Mazda");
-		System.out.println(cars);
+		dogs.addLast("Cane Corso");
+		System.out.println("All breeds after addition at last element");
+		printAllelements(dogs);	
+		newLine();
 		
 		//use removeLast() to remove the last item on the list
-		cars.removeLast();
-		System.out.println(cars);
+		dogs.removeLast();
+		System.out.println("All breeds after removal at last element");
+		printAllelements(dogs);
 		
 
 	}
+	
+	public static void newLine() {
+		System.out.println("");
+	}
+	
+	public static void printAllelements(LinkedList<String> ll) {
+		System.out.println(ll);
+	}
 
 }
+
